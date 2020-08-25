808c908b7065983f65df39bd670ef518d0dbdd84
Mburu-coder <60397100+Mburu-coder@users.noreply.github.com>
2020-08-25 19:57:24 +0300

    Update sandbox.js
    Adding raw csv link

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('/csv/Kenya.csv', function(err, rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/Mburu-coder/Animated_plotly.js/master/csv/Kenya.csv', function(err, rows){
 
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
